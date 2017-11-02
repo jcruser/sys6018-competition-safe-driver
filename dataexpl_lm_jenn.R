c275bddc1f22e861be17ec6bb9c00e99caf8838f
jcruser <jc4pg@virginia.edu>
2017-11-02 08:06:58 -0400

    added kfold log reg

--- dataexpl_lm_jenn.R
+++ dataexpl_lm_jenn.R
@@ -6,23 +6,40 @@ library(dplyr)
 
 train <- read.csv("train.csv")
 test <- read.csv("test.csv")
-#sample <- read.csv("sample_submission.csv")
+
+### Data Cleaning and Exploration #####
 
 #Treat the categorical vars as factors
 cats <- names(train)[grepl('_cat$', colnames(train))]
 bins <- names(train)[grepl('_bin$', colnames(train))]
 apply(train[,cats],2,as.factor)
 apply(train[,bins],2,as.factor)
-
 cats <- names(test)[grepl('_cat$', colnames(test))]
 bins <- names(test)[grepl('_bin$', colnames(test))]
 apply(test[,cats],2,as.factor)
 apply(test[,bins],2,as.factor)
 
 #Treat missing values
-#train[train==-1] <- NA
-#test[test==-1] <- NA
+#Calculate number of missing values in each column
+Missings <- apply(train,2,FUN=function(x) length(which(x==-1)))
+Missings
+#ps_car_03_cat, ps_car_05_cat and ps_reg_03 are missing too many values
+dropvar <- c("ps_car_03_cat","ps_car_05_cat","ps_reg_03")
+train.clean <- train[,-which(names(train) %in% dropvar)]
+Missings <- apply(train.clean,2,FUN=function(x) length(which(x==-1)))
+Missings
+#Impute missing values for ps_car_14,ps_car_07_cat and ps_ind_05_cat
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+train.clean[train.clean$ps_car_14==-1,]$ps_car_14 = mean(train.clean[train.clean$ps_car_14!=-1,]$ps_car_14)
+train.clean[train.clean$ps_ind_05_cat==-1,]$ps_ind_05_cat = Mode(train.clean[train.clean$ps_ind_05_cat!=-1,]$ps_ind_05_cat)
+train.clean[train.clean$ps_car_07_cat==-1,]$ps_car_07_cat = Mode(train.clean[train.clean$ps_car_07_cat!=-1,]$ps_car_07_cat)
 
+
+##### basic lm #####
+#not the right type of model but starts to give insights on some of the vars
 basic.lm <- lm(target~.-id,data=train)
 summary(basic.lm)
 
@@ -46,7 +63,7 @@ summary(basic.lm)
 #ps_calc_19_bin
 
 #######  model exploring sig values #########
-
+#still an lm so not the right model
 sigvals.lm <- lm(target~ ps_ind_03 + ps_ind_04_cat + ps_ind_05_cat + ps_ind_06_bin + ps_ind_07_bin + ps_ind_08_bin + ps_ind_15 + ps_ind_16_bin+ 
                    ps_ind_17_bin + ps_ind_18_bin + ps_reg_02 + ps_car_03_cat + ps_car_07_cat + ps_car_11 + ps_car_12+ps_car_13+ps_calc_19_bin, data=train )
 
@@ -83,7 +100,7 @@ plot(yhat2,sigvalsresid2)
 
 
 ############## Log Regression #######################
-
+## using the same sign vals as the previous model
 basic.glm = glm(target~ps_ind_03 + ps_ind_04_cat + ps_ind_05_cat + ps_ind_07_bin + ps_ind_08_bin + ps_ind_15 + ps_ind_16_bin+ 
                   ps_ind_17_bin + ps_ind_18_bin + ps_reg_02 + ps_car_03_cat + ps_car_07_cat + ps_car_11 + ps_car_12+ps_car_13, data = train, family='binomial')
 
@@ -109,28 +126,60 @@ colnames(log_preds)[1] <- "id"
 #write to csv
 write.table(log_preds, file = "c:/Users/Jennifer/Desktop/SafeDriveLog.csv", row.names=F, col.names=T, sep=",")
 
+######## Log Reg with kfold ############
+
+library(caret)
+
+# define training control
+train_control<- trainControl(method="cv", number=10)
+
+# train the model 
+kfold_logr<- train(target~ps_ind_03 + ps_ind_04_cat + ps_ind_05_cat + ps_ind_07_bin + ps_ind_15 + ps_ind_16_bin+ ps_ind_17_bin + ps_ind_18_bin + 
+                     ps_reg_02 + ps_car_07_cat + ps_car_11 + ps_car_12+ps_car_13, data=train.clean, trControl=train_control, 
+                   method="glm", family=binomial())
+
+# print cv scores
+summary(kfold_logr)
+
+#look at results
+kfold_logr$results
+
+## new log reg with more limited var
+
+kfold.glm = glm(target~ps_ind_03 + ps_ind_04_cat + ps_ind_05_cat + ps_ind_07_bin + ps_ind_15 + ps_ind_16_bin+ ps_ind_17_bin + ps_ind_18_bin + 
+                  ps_reg_02 + ps_car_07_cat + ps_car_11 + ps_car_12+ps_car_13, data = train.clean, family='binomial')
+
+
+## apply model to test data
+probs <- predict(kfold.glm, newdata = test, type = "response")
+mypreds <- cbind(as.character(test$id),probs)
+
+
+#write to csv
+write.table(mypreds, file = "c:/Users/Jennifer/Desktop/SafeDriveLogk.csv", row.names=F, col.names=T, sep=",")
+
 
 ###### apply gini index function ######
 #' Calculates unnormalized Gini index from ground truth and predicted probabilities.
 #' @param ground.truth Ground-truth scalar values (e.g., 0 and 1)
 #' @param predicted.probabilities Predicted probabilities for the items listed in ground.truth
 #' @return Unnormalized Gini index.
-unnormalized.gini.index = function(target, probs_log) {
+unnormalized.gini.index = function(ground.truth, predicted.probabilities) {
   
-  if (length(target) !=  length(probs_log))
+  if (length(ground.truth) !=  length(predicted.probabilities))
   {
     stop("Actual and Predicted need to be equal lengths!")
   }
   
   # arrange data into table with columns of index, predicted values, and actual values
-  gini.table = data.frame(index = c(1:length(target)), probs_log, target)
+  gini.table = data.frame(index = c(1:length(ground.truth)), predicted.probabilities, ground.truth)
   
   # sort rows in decreasing order of the predicted values, breaking ties according to the index
-  gini.table = gini.table[order(-gini.table$probs_log, gini.table$index), ]
+  gini.table = gini.table[order(-gini.table$predicted.probabilities, gini.table$index), ]
   
   # get the per-row increment for positives accumulated by the model 
-  num.ground.truth.positivies = sum(gini.table$target)
-  model.percentage.positives.accumulated = gini.table$target / num.ground.truth.positivies
+  num.ground.truth.positivies = sum(gini.table$ground.truth)
+  model.percentage.positives.accumulated = gini.table$ground.truth / num.ground.truth.positivies
   
   # get the per-row increment for positives accumulated by a random guess
   random.guess.percentage.positives.accumulated = 1 / nrow(gini.table)
@@ -145,10 +194,10 @@ unnormalized.gini.index = function(target, probs_log) {
 #' @param ground.truth Ground-truth scalar values (e.g., 0 and 1)
 #' @param predicted.probabilities Predicted probabilities for the items listed in ground.truth
 #' @return Normalized Gini index, accounting for theoretical optimal.
-normalized.gini.index = function(target, probs_log) {
+normalized.gini.index = function(ground.truth, predicted.probabilities) {
   
-  model.gini.index = unnormalized.gini.index(target, probs_log)
-  optimal.gini.index = unnormalized.gini(target, target)
+  model.gini.index = unnormalized.gini.index(ground.truth, predicted.probabilities)
+  optimal.gini.index = unnormalized.gini(ground.truth, ground.truth)
   return(model.gini.index / optimal.gini.index)
 }
 
